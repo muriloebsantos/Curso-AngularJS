071a609e90d531606e633dee7c836e44557b92a1
= <=>
2017-09-07 22:05:01 -0300

    criado eventos para adicionar e remover item da lista - diretiva ng-click e utilizado par (key, tarefa) no ng-repeat

--- Aula2/script.js
+++ Aula2/script.js
@@ -7,8 +7,23 @@ MinhaLista.controller('TarefasController', function ($scope) {
         'prioridade': 1
      },
      {
-       'nome': "Ir a faculidade",
+       'nome': "Ir a faculdade",
        'prioridade': 2
      }
    ];
+
+   $scope.add = function () {
+      var tarefa = {
+        'nome': $scope.nome,
+        'prioridade': $scope.prioridade
+      };
+      $scope.tarefas.push(tarefa);
+      $scope.nome = '';
+      $scope.prioridade = '';
+   };
+
+   $scope.delete = function(i) {
+      $scope.tarefas.splice(i, 1);
+   };
+
 });
