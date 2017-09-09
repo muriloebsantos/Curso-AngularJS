7e1c54a224f9d3422e6adb7c0add04c38ac1b10d
= <=>
2017-09-09 15:50:22 -0300

    criacao de um filtro personalizado

--- Aula2/script.js
+++ Aula2/script.js
@@ -1,5 +1,23 @@
 var MinhaLista = angular.module('MinhaLista', []);
 
+MinhaLista.filter('reverse', function(){
+   return function(input, end, lowercase){
+     input = input || '';
+     out = '';
+     for (var i = 0; i < input.length; i++) {
+       out = input.charAt(i) + out;
+     }
+
+     end = end || '...';
+     out = out + end;
+     if(lowercase){
+       out = out.toLowerCase();
+     }
+     return out;
+   }
+
+});
+
 MinhaLista.controller('TarefasController', function ($scope) {
    $scope.tarefas = [
      {
