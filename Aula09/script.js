81a80c506ab8d1310dfd67c9b8ee5ba74abdde56
= <=>
2017-09-09 16:40:49 -0300

    diretiva de template html (Evitar usar muito htm no JS)

--- Aula09/script.js
+++ Aula09/script.js
@@ -3,3 +3,9 @@ var diretivas = angular.module('Diretivas', []);
 diretivas.controller('DiretivasController', function($scope){
   $scope.dev = 'Murilo E B Santos';
 });
+
+diretivas.directive('efDev', function() {
+   return {
+     template: "<strong>Desenvolvedor:</strong> {{dev}}"
+   }
+});
