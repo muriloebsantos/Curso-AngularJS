7108d6b4100f1d74d07d9eda973adfd801314c5e
unknown <=>
2017-09-12 14:00:24 -0300

    criando rotas

--- CRUD/script.js
+++ CRUD/script.js
@@ -1,1 +1,21 @@
 var usuarios = angular.module("Usuarios", ['ngRoute']);
+
+usuarios.config(['$routeProvider', function($routeProvider){
+  $routeProvider
+      .when('/usuarios', {
+         templateUrl: 'partials/home.html',
+         controller: 'UsuariosController'
+      })
+      .when('/usuarios/novo', {
+         templateUrl: 'partials/novo.html',
+         controller: 'UsuariosController'
+      })
+      .when('/usuarios/editar/:id', {
+         templateUrl: 'partials/editar.html',
+         controller: 'UsuariosController'
+      })
+}]);
+
+usuarios.controller('UsuariosController', ['$scope', function($scope){
+
+}]);
