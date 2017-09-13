3fe9036b295c2e54fd536cf8f932f6d715abe0ae
unknown <=>
2017-09-13 13:43:37 -0300

    exemplo crud (nao testado)

--- CRUD/script.js
+++ CRUD/script.js
@@ -1,4 +1,4 @@
-var usuarios = angular.module("Usuarios", ['ngRoute']);
+var usuarios = angular.module("Usuarios", ['ngRoute', 'ngResource']);
 
 usuarios.config(['$routeProvider', function($routeProvider){
   $routeProvider
@@ -16,6 +16,49 @@ usuarios.config(['$routeProvider', function($routeProvider){
       })
 }]);
 
-usuarios.controller('UsuariosController', ['$scope', function($scope){
+usuarios.factory('UsuarioModel', ['$resource', function($resource){
+    return $resource('http://localhost/server-resources/usuarios/:id',
+                      null,
+                      {
+                          'update'  : {
+                             method: 'PUT'
+                          }
+                      }
+      );
+}]);
+usuarios.controller('UsuariosController', ['$scope', 'UsuarioModel', '$routeParams', function($scope){
+   $scope.usuarios = UsuarioModel.query();
+
+   $scope.ver = function(id) {
+     $scope.usuario = UsuarioModel.get({id:$routeParams.id});
+   }
+
+   $scope.salvar = function() {
+      UsuarioModel.update({id: $routeParams.id},
+                           $scope.usuario,
+                           function(){
+                              if(confirm("Voltar?"))
+                                 $location.path('/usuarios');
+                           },
+                         function(data, status){
+                             alert('Erro ' + status);
+                         });
+   }
+
+   $scope.remover = function(id) {
+		if (confirm('Tem certeza?')) {
+			UsuarioModel.remove(
+				{id:id},
+				{},
+				function() {
+					alert('Removido com sucesso!');
+					$scope.usuarios = UsuarioModel.query();
+				},
+				function(data, status) {
+					alert('Error '+status);
+				}
+			);
+		}
+	}
 
 }]);
